2ab6398e07596ea344ebc417704b596bcbde60ff
Adolf45 <94709695+Adolf45@users.noreply.github.com>
2021-11-27 13:49:07 -0600

    Add files via upload

---
+++ src/Components/Foods/data.js
@@ -0,0 +1,34 @@
+import product1 from '../../../src/assets/images/Flautas.jpg';
+import product2 from '../../../src/assets/images/TacosSuadero.jpg';
+import product3 from '../../../src/assets/images/Tamales.jpg';
+
+
+export const productData = [
+  {
+    img: product1,
+    alt: 'Flautas',
+    name: 'Deliciosas Flautas',
+    desc:
+      'Deliciosas Flautas de Cerdo, Orden: ',
+    price: '$30.00',
+    button: 'Ordenar'
+  },
+  {
+    img: product2,
+    alt: 'Tacos',
+    name: 'Tacos',
+    desc:
+      ' Exquisitos Tacos de Suadero, Orden: ',
+    price: '$20.00',
+    button: 'Ordenar'
+  },
+  {
+    img: product3,
+    alt: 'Tamalitos',
+    name: 'Tamales Tradicionales ',
+    desc:
+      ' Exquisitos Tamales de Pollo, Orden: ',
+    price: '$25.00',
+    button: 'Ordenar'
+  }
+];
---
+++ src/Components/Foods/index1.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import {
+  ProductsContainer,
+  ProductWrapper,
+  ProductsHeading,
+  ProductTitle,
+  ProductCard,
+  ProductImg,
+  ProductInfo,
+  ProductDesc,
+  ProductPrice,
+  ProductButton
+} from './FoodsElements';
+import Link from '@material-ui/core/Link';
+
+const Products = ({ heading, data }) => {
+  return (
+    <ProductsContainer>
+      <ProductsHeading>{heading}</ProductsHeading>
+      <ProductWrapper>
+        {data.map((product, index) => {
+          return (
+            <ProductCard key={index}>
+              <ProductImg src={product.img} alt={product.alt} />
+              <ProductInfo>
+                <ProductTitle>{product.name}</ProductTitle>
+                <ProductDesc>{product.desc}</ProductDesc>
+                <ProductPrice>{product.price}</ProductPrice>
+                <Link href="/order"><ProductButton>{product.button}</ProductButton></Link>
+              </ProductInfo>
+            </ProductCard>
+          );
+        })}
+      </ProductWrapper>
+    </ProductsContainer>
+  );
+};
+
+export default Products;
